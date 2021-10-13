d42a61e9b0d2bb500df3e83bf7cdd096bac400e7
breadfan18 <breadfan18@gmail.com>
2021-10-13 18:51:31 -0400

    initial comming for today

--- newDomMenuLab/js/script.js
+++ newDomMenuLab/js/script.js
@@ -1,12 +1,35 @@
-const menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+var menuLinks = [
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+]
 
+// Task 1 - 3
 let mainEl = document.querySelector('main');
-
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 mainEl.classList.add('flex-ctr');
@@ -17,11 +40,49 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 topMenuEl.classList.add('flex-around')
 
 menuLinks.forEach(link => {
-    let newA = document.createElement('a');
-    newA.setAttribute('href', link.href);
-    newA.innerText = link.text;
-    topMenuEl.appendChild(newA)
+  let newA = document.createElement('a');
+  newA.setAttribute('href', link.href);
+  newA.innerText = link.text;
+  topMenuEl.appendChild(newA)
+})
+
+// Task 4
+let subMenuEl = document.querySelector('#sub-menu');
+subMenuEl.style.height = '100%';
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+subMenuEl.classList.add('flex-around');
+subMenuEl.style.position = 'absolute'
+subMenuEl.style.top = '0';
+
+// Task 5
+let allAElements = document.querySelectorAll('#top-menu  a');
+let showingSubMenu = false;
+
+topMenuEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  let currentElement = e.target;
+  if(currentElement.tagName !== 'A') return;
+  if(currentElement.classList.contains('active')){
+    currentElement.classList.remove('active');
+    showingSubMenu = false;
+    subMenuEl.style.top = 0;
+    
+  }
 })
 
 
-let test = 'test'
+function letters(array1){
+  let returnArr = [];
+  for (let i = 0; i < array1.length; i++) {
+    const currentElement = array1[i];
+    for (let j = 1; j < array.length; j++) {
+      
+      
+    }
+
+    
+    
+  }
+}
+
+console.log(letters(['a', 'b', 'c']));

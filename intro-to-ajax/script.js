7001fea521f5cf96ef3b5ed2eafc7311897a132f
breadfan18 <breadfan18@gmail.com>
2021-10-23 12:52:18 -0400

    some code cleanup

--- intro-to-ajax/script.js
+++ intro-to-ajax/script.js
@@ -4,14 +4,12 @@ const API_KEY = '6ddc69a0';
 
 
 // Variables - data that changes
-
+let movieData;
 
 // Cached elements
-const $title = $('#title');
-const $year = $('#year');
-const $rated = $('#rated');
 const $form = $('form');
 const $input = $('input[type="text"]');
+const $main = $('main')
 
 // Event Listeners
 $form.on('submit', handleSubmit)
@@ -26,9 +24,8 @@ function handleSubmit(e) {
         .then(
             // success callback 
             function (data) {
-                $title.text(data.Title)
-                $year.text(data.Year)
-                $rated.text(data.Rated)
+                movieData = data;
+                render();
             },
             // failure callback
             function (error) {
@@ -37,6 +34,19 @@ function handleSubmit(e) {
         )
 }
 
+function render() {
+    $main.html(
+        `
+        <img src='${movieData.Poster}' alt='${movieData.Title}'' />
+        <h3>Title</h3>
+        <p id="title">${movieData.Title}</p>
+        <p>Year</p>
+        <p id="year">${movieData.Year}</p>
+        <p>Rating</p>
+        <p id="rated">${movieData.Rated}</p>
+        `
+    )
+}
 
 // Using jQuery's $.ajax() method - we use it to request information from a resource using ajax
 // --> this method when called returns a javaScript promise object 

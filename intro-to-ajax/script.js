825c812027dfa4e9de7b13436dae9083029c82bb
breadfan18 <breadfan18@gmail.com>
2021-10-23 11:38:16 -0400

    added some html for movie

--- intro-to-ajax/script.js
+++ intro-to-ajax/script.js
@@ -1,1 +1,31 @@
-alert('JS loaded')
+// Constants - data that does NOT change 
+
+
+// Variables - data that changes
+
+
+// Cached elements
+
+
+// Event Listeners
+
+
+// Functions 
+
+
+// Using jQuery's $.ajax() method - we use it to request information from a resource using ajax
+// --> this method when called returns a javaScript promise object 
+
+
+
+$.ajax('https://www.omdbapi.com/?apikey=6ddc69a0&t=Frozen')
+    .then(
+        // success callback 
+        function (data) {
+            console.log(data);
+        },
+        // failure callback
+        function (error) {
+            console.log('BAD REQUEST', error)
+        }
+    )

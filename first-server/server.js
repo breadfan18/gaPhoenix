58933f8af7d6456f0be6e48231906fc3f8f1ddf3
breadfan18 <breadfan18@gmail.com>
2021-11-06 12:55:45 -0400

    added express boilerplate

---
+++ first-server/server.js
@@ -0,0 +1,24 @@
+// this file is a module AKA node module
+// server.js is always the entry point of our application 
+// this is the first js file that gets executed when our server runs
+// this is where we configure settings and anything else our server needs 
+
+
+// require dependencies
+const express = require('express'); //we use require statements to include code from a node module 
+
+
+// initialize this application
+const app = express();
+
+
+// configure application settings
+
+
+// configure our "mount" middleware
+
+
+// define our routes
+
+
+// tell the application to listen for requests from the client or the browser

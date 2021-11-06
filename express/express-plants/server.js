adf434a58278470b85098c9ab1697766168ff806
breadfan18 <breadfan18@gmail.com>
2021-11-06 16:32:32 -0400

    routes defined

---
+++ express/express-plants/server.js
@@ -0,0 +1,32 @@
+// Require dependencies
+const express = require('express');
+const plants = [
+    "Monstera Deliciosa",
+    "Corpse Flower",
+    "Elephant-Foot Yam",
+    "Witches' Butter",
+  ]
+
+
+// Initialize the app
+const app = express();
+
+
+// Configure App Settings
+
+
+// Setup a port value
+const port = 3000;
+
+
+// Mount middleware
+
+
+// Mount routes
+app.get('/:indexOfPlantsArray', (req, res) => {
+    res.send(plants[req.params.indexOfPlantsArray]);
+})
+
+
+// Tell the app to listen
+app.listen(port, () => console.log('Express is listening at port ' + port))

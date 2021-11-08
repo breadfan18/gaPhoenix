93e98daaa8bd5ead40ffdc376175d7ec2c783641
breadfan18 <breadfan18@gmail.com>
2021-11-08 10:32:20 -0500

    greeting route

--- express/express-lab-2/server.js
+++ express/express-lab-2/server.js
@@ -8,8 +8,13 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Welllll')
 })
+
 app.get('/greeting', (req, res) => {
     res.send('Hello Stranger');
+})
+
+app.get('/greeting/:name', (req, res) => {
+    res.send(`Hello there, ${req.params.name}`);
 })
 
 

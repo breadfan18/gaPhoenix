03577420fc0112daf631b7a07404ab96b5f24afd
breadfan18 <breadfan18@gmail.com>
2021-11-08 10:41:26 -0500

    8 ball done

--- express/express-lab-2/server.js
+++ express/express-lab-2/server.js
@@ -1,7 +1,7 @@
 // require express and set up the app 
 const express = require('express');
 const app = express();
-
+const eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
 
 // Routes 
@@ -21,6 +21,10 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`Total tip: ${req.params.total * (req.params.tipPercentage/100)}`);
 })
 
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+    res.send(`Magic 8 ball response: <h1>${eightBall[Math.floor(Math.random()*eightBall.length)]}</h1>`);
+})
 
+x
 // Listening
 app.listen(3000, () => console.log('Express lab 2 is lisntening on port 3000'));

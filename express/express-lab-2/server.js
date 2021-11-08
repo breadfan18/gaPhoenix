d3b663e9318a971e8dd9b76107439b7e8a1f2a01
breadfan18 <breadfan18@gmail.com>
2021-11-08 10:25:59 -0500

    listener set

---
+++ express/express-lab-2/server.js
@@ -0,0 +1,11 @@
+// require express and set up the app 
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+// Listening
+app.listen(3000, () => console.log('Express lab 2 is lisntening on port 3000'));

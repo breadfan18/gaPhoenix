a33fd89132023584148d764930153db296e1e5d3
breadfan18 <breadfan18@gmail.com>
2021-11-09 19:36:45 -0500

    updated fruits array

--- express/fruits/server.js
+++ express/fruits/server.js
@@ -1,12 +1,53 @@
 const express = require("express")
 const app = express()
 
-const fruits = ["apple", "banana", "pear"]
+const fruits = [
+    {
+        id: 1,
+        name: 'apple',
+        color: 'blue',
+        readyToEat = true
+    },
+    {
+        id: 2,
+        name: 'apple',
+        color: 'green',
+        readyToEat = false
+    },
+    {
+        id: 3,
+        name: 'orange',
+        color: 'orange',
+        readyToEat = false
+    },
+    {
+        id: 4,
+        name: 'banana',
+        color: 'green',
+        readyToEat = false
+    },
+    {
+        id: 5,
+        name: 'pineapple',
+        color: 'yellow',
+        readyToEat = true
+    },
 
+]
+
+// Index
 app.get("/fruits/", (req, res) => {
-  res.send(fruits)
+    res.send(fruits)
 })
 
+// Show
+app.get("/fruits/:indexOfFruit", (req, res) => {
+    res.send(fruits[req.params.indexOfFruit])
+})
+
+
+
+// App listener
 app.listen(3000, () => {
-  console.log("listening")
+    console.log("listening")
 })

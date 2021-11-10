d66fb8be39aa05b0fa5f792d70c69bf10c3682dc
breadfan18 <breadfan18@gmail.com>
2021-11-09 20:57:59 -0500

    added mvc pattern to fruits

---
+++ express/fruits/models/fruits.js
@@ -0,0 +1,33 @@
+const fruits = [
+    {
+        id: 1,
+        name: 'apple',
+        color: 'blue',
+        readyToEa: true
+    },
+    {
+        id: 2,
+        name: 'apple',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        id: 3,
+        name: 'orange',
+        color: 'orange',
+        readyToEat: false
+    },
+    {
+        id: 4,
+        name: 'banana',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        id: 5,
+        name: 'pineapple',
+        color: 'yellow',
+        readyToEat: true
+    },
+
+]

de66860aaf6cdafbf2d787139a8c83ce5ff7b4a2
breadfan18 <breadfan18@gmail.com>
2021-11-10 19:11:39 -0500

    working on crud bro

--- express/fruits/server.js
+++ express/fruits/server.js
@@ -10,6 +10,15 @@ app.get("/fruits/", (req, res) => {
     })
 })
 
+// New 
+app.get('/fruits/new', (req, res) => {
+    res.render('new.ejs');
+})
+
+app.post('/fruits', (req, res) => {
+    res.send('hi')
+})
+
 // Show
 app.get("/fruits/:id", (req, res) => {
     const foundFruit = fruits.find(fruit => fruit.id === parseInt(req.params.id));

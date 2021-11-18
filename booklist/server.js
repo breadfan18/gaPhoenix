fe0f39762c3534c0f49a7372ff9ed2a9084bc4e2
breadfan18 <breadfan18@gmail.com>
2021-11-17 19:24:51 -0500

    app basic framework done

---
+++ booklist/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+// initialize app
+const app = express();
+
+// Configure settings
+require('dotenv').config();
+
+
+// Connect to and configure mongoDB with mongoose
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+// setup mongodb event listeners
+db.on('connected', () => console.log('Connected to MongoDB'));
+db.on('error', (err) => console.log('Mongo error: ' + err.message));
+
+
+// Mount middleware
+app.use(express.urlencoded({extended: true}));
+
+
+// Mount Routes
+
+
+
+// Tell the app to listen
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log('App is listening on Port ' + PORT))

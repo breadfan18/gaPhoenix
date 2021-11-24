deee967af9291d7a61850920a532cec55db9ada8
breadfan18 <breadfan18@gmail.com>
2021-11-23 19:28:07 -0500

    testing

---
+++ intro-to-auth/controllers/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+const User = require('../models/user');
+
+usersRouter.get('/users', (req, res) => {
+    User.find({}, (err, users) => {
+        res.render('home.ejs');
+    })
+})

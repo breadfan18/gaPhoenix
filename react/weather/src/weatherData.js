f36fc11640c73bd0a3b01ad18b149437092cb55c
breadfan18 <breadfan18@gmail.com>
2021-12-18 16:58:49 -0500

    setup weather data

---
+++ react/weather/src/weatherData.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        img: "https://images.unsplash.com/photo-1536514072410-5019a3c69182?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        conditions: 'sunny',
+        time: 'day'
+    },
+    {
+        img: "https://images.unsplash.com/photo-1536514072410-5019a3c69182?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        conditions: 'clear',
+        time: 'day'
+    },
+    {
+        img: "https://images.unsplash.com/photo-1536514072410-5019a3c69182?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        conditions: 'cloudy',
+        time: 'day'
+    },
+    {
+        img: "https://images.unsplash.com/photo-1536514072410-5019a3c69182?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        conditions: 'partly sunny',
+        time: 'night'
+    },
+    {
+        img: "https://images.unsplash.com/photo-1536514072410-5019a3c69182?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        conditions: 'rainy',
+        time: 'day'
+    } 
+];
+
+export default data;

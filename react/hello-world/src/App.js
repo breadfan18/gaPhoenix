1cfee9a4757625597aa3597d4295c47bda8dd03b
breadfan18 <breadfan18@gmail.com>
2021-12-18 11:34:29 -0500

    little here little there

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+// Components return a part or all of the UI
+// A component receives data as input and renders that data as a user interface
+// Input data, Output view. 
 function App() {
   return (
-    <h1>Hello React World</h1>
+    //When empty jsx tags are used as wrappers, they are called 'jsx fragments'. 
+    <>
+      <h1>Hello React World</h1>
+    </>
   );
 }
 

31addae3fa23f47fafc2dff27097adf5e2114bc5
breadfan18 <breadfan18@gmail.com>
2021-12-21 19:20:39 -0500

    removed some files

---
+++ react/counter/src/Counter.js
@@ -0,0 +1,13 @@
+const Counter = () => {
+    return (
+        <>
+            <span>Current Count: 0</span>
+            <section>
+                <button>+</button>
+                <button>-</button>
+            </section>
+        </>
+    )
+}
+
+export default Counter;

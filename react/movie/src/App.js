267d4a8a8cebb6fe1844ac4b3a15db083769bed0
breadfan18 <breadfan18@gmail.com>
2021-12-22 19:07:31 -0500

    added some compoenents

--- react/movie/src/App.js
+++ react/movie/src/App.js
@@ -1,12 +1,15 @@
 // IMPORT useState
 import React, { useState } from "react";
+import Form from "./components/Form";
+import MovieDisplay from "./components/MovieDisplay";
 import "./index.css";
 
 export default function App() {
 
   return (
     <div className="App">
-      <h1>Template</h1>
+      <Form />
+      <MovieDisplay />
     </div>
   );
 }
---
+++ react/movie/src/components/MovieDisplay.js
@@ -0,0 +1,6 @@
+const MovieDisplay = (props) => {
+    return <h1>The MovieDisplay Component</h1>;
+  };
+  
+  
+export default MovieDisplay;

08d504b23189ea9895b33c0ecf211d978ed43852
breadfan18 <breadfan18@gmail.com>
2021-12-22 22:24:47 -0500

    done with giphy project

--- react/giphy/src/components/Form.js
+++ react/giphy/src/components/Form.js
@@ -10,7 +10,7 @@ const Form = ({ getGif }) => {
         <>
             <h1>Click the Button to get a Random Gif</h1>
             <form onSubmit={handleSubmit}>
-                <input type="submit" value="Click for a Gif" />
+                <input type="submit" value="Click Me" />
 
             </form>
         </>

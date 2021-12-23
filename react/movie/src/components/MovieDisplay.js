156b919b90b3dddcf5ec69baa434dc5003c26ba3
breadfan18 <breadfan18@gmail.com>
2021-12-22 20:37:13 -0500

    updated movie display component

--- react/movie/src/components/MovieDisplay.js
+++ react/movie/src/components/MovieDisplay.js
@@ -1,14 +1,19 @@
-const MovieDisplay = (props) => {
-
-  console.log('tutu tara', props.movie);
+const MovieDisplay = ({ movie }) => {
+  const loaded = () => {
     return (
       <>
-        <h1>{props.movie.Title}</h1>
-        <img src={props.movie.Poster} alt="" />
+        <h1>{movie.Title}</h1>
+        <h2>{movie.Genre}</h2>
+  
+        <img src={movie.Poster} alt={movie.Title} />
       </>
     )
-    
-  };
-  
-  
+  }
+
+  const loading = () => <h1>No Film to Display</h1>
+
+  return movie ? loaded() : loading()
+};
+
+
 export default MovieDisplay;

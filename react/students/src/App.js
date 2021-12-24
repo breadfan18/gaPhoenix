25cfdf5d50fb95d27b3fbf8e3b10f50c901ebe0c
breadfan18 <breadfan18@gmail.com>
2021-12-24 16:41:31 -0500

    done with students

--- react/students/src/App.js
+++ react/students/src/App.js
@@ -3,18 +3,25 @@ import React, { useState } from "react";
 import { students } from "./students";
 import "./index.css";
 import Student from "./Student";
+import Score from "./Score";
 
 
 export default function App() {
-  
-  const [ allStudents, setAllStudenta ] = useState(students);
 
-  const studentComponents = allStudents.map((student, index) => {
-    return <Student 
-      key={index}
-      student={student}
-    />
-  })  
+  const [allStudents, setAllStudenta] = useState(students);
+
+  const studentComponents = allStudents.map((student, index) => (
+    <>
+      <Student
+        key={index}
+        student={student}
+      />
+      <Score
+        key={'score' + index}
+        scores={student.scores}
+      />
+    </>)
+  )
   return (
     <div className="App">
       <h1>React Students</h1>
--- react/students/src/Score.js
+++ react/students/src/Score.js
@@ -1,6 +1,23 @@
-const Score = () => {
-    return(
-        <h2>Score</h2>
+const Score = ({ scores }) => {
+    const scoreElements = scores.map(score =>
+        (<table style={{ border: '1px solid black'}}>
+            <tbody>
+                <tr style={{ backgroundColor: 'black', color: 'white'}}>
+                    <th>Date</th>
+                    <th>Score</th>
+                </tr>
+                <tr style={{ border: '1px solid black'}}>
+                    <td>{score.date}</td>
+                    <td>{score.score}</td>
+                </tr>
+            </tbody>
+        </table>
+        )
+    )
+    return (
+        <div className="tableDiv">
+            {scoreElements}
+        </div>
     )
 }
 

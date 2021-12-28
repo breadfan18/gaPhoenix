5968301eaf855e0ee81b94861a7250bfc90ba668
breadfan18 <breadfan18@gmail.com>
2021-12-27 22:56:23 -0500

    last little bit

--- react/students/src/Student.js
+++ react/students/src/Student.js
@@ -1,13 +1,12 @@
 import Score from "./Score";
 
 const Student = ({ student, key }) => {
-    console.log(student);
     return (
         <div className="card" style={{ width: "18rem" }}>
             <img src={student.img} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{student.name}</h5>
-                <p className="card-text" style={{height: "25rem"}}>{student.bio}</p>
+                <p className="card-text" style={{ height: "25rem" }}>{student.bio}</p>
             </div>
             <Score
                 key={'score' + key}

5a1d431bf467e6d803e36dfaedbb5f9fc4045f30
breadfan18 <breadfan18@gmail.com>
2022-01-05 15:39:43 -0500

    changes for the day

--- express/turtles-api/server.js
+++ express/turtles-api/server.js
@@ -1,6 +1,7 @@
 // Instantiate express app
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Initialize the app
 const app = express();
@@ -8,7 +9,6 @@ const app = express();
 //Configure settings
 require('dotenv').config();
 const {DATABASE_URL, PORT = 3000} = process.env;
-
 
 //Connect with mongo
 mongoose.connect(DATABASE_URL);
@@ -19,12 +19,11 @@ db.on('connected', () => console.log('Connected to MongoDB'));
 db.on('error', (error) => console.log(`Mongo Error: ${error.message}`));
 
 // Mount middleware
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(express.json()) //this will create req.body with incoming JSON
 app.use('/api/skills', require('./controllers/skills'));
-
-
 
 // Listener
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))

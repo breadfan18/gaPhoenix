437ce3c23a10f48a68c608dbedee421818d713ce
breadfan18 <breadfan18@gmail.com>
2022-01-05 19:28:05 -0500

    working on crypto stuff

---
+++ react/cryptoprices/pages/Currencies.js
@@ -0,0 +1,5 @@
+const Currencies = (props) => {
+    return <h1>This is the Currencies page</h1>
+}
+
+export default Currencies;
---
+++ react/cryptoprices/pages/Price.js
@@ -0,0 +1,5 @@
+const Price = (props) => {
+    return <h1>This is the price page</h1>
+}
+
+export default Price;

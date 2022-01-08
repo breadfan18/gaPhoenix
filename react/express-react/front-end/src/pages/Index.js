1fb7bf7ca6555a051b155eec32d499badace0103
breadfan18 <breadfan18@gmail.com>
2022-01-08 16:07:41 -0500

    people are rendered

--- react/express-react/front-end/src/pages/Index.js
+++ react/express-react/front-end/src/pages/Index.js
@@ -1,5 +1,24 @@
-const Component = (props) => {
-    return <h1>Component Name</h1>
+import { Link } from 'react-router-dom';
+
+
+const Index = ({ people, createPeople }) => {
+    const loaded = () => {
+        return people.map(person => (
+            <div className='person' key={person._id}>
+                <Link to={`people/${person._id}`} >
+                    <h1>{person.name}</h1>
+                </Link>
+                <img style={{height: '10rem', width: '20rem'}} src={person.image} alt={person.name} />
+                <h3>{person.title}</h3>
+            </div>
+        ))
+    }
+
+    const loading = () => <h1>Loading...</h1>
+
+    return people ? loaded() : loading()
 }
 
-export default Component
+
+
+export default Index

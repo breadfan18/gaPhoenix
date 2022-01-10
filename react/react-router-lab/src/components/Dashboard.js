f2a28c62ebd94a774e10e68616bba584e085b2bf
breadfan18 <breadfan18@gmail.com>
2022-01-10 14:42:36 -0500

    setup a table for stock dashboard

--- react/react-router-lab/src/components/Dashboard.js
+++ react/react-router-lab/src/components/Dashboard.js
@@ -1,18 +1,36 @@
 import { Link } from 'react-router-dom';
 
-const About = ({stocks}) => {
+const About = ({ stocks }) => {
     const stockElements = stocks.map((stock, index) => (
-        <div className="stock" key={index}>
-            <Link to={`stocks/${stock.symbol}`}>
-                {stock.name}
-            </Link>
-        </div>
+        <tr className="stock" key={index}>
+            <td>
+                <Link to={`stocks/${stock.symbol}`}>
+                    {stock.name}
+                </Link>
+            </td>
+            <td>{stock.lastPrice}</td>
+            <td>{stock.change}</td>
+            <hr />
+        </tr>
+
+
     ))
 
     return (
         <div>
             <h1>Available Stocks</h1>
-            <div >{stockElements}</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Company Name</th>
+                        <th>Price</th>
+                        <th>Change</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {stockElements}
+                </tbody>
+            </table>
         </div>
     )
 }

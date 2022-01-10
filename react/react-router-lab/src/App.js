3a8a668387b88901d0f170549195f5dd2617b635
breadfan18 <breadfan18@gmail.com>
2022-01-10 14:22:14 -0500

    basic react router lab done

--- react/react-router-lab/src/App.js
+++ react/react-router-lab/src/App.js
@@ -1,12 +1,35 @@
 // IMPORT useState
 import React, { useState } from "react";
-import stocks from './stockData';
+import { Route, Switch } from "react-router-dom"
+import allStocks from './stockData';
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import Stock from './components/Stock';
+import Header from './components/Header';
 
 export default function App() {
+  const [ stocks, setAllStocks ]  = useState(allStocks);
+
 
   return (
     <div className="App">
-      <h1>Template</h1>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route path="/stocks/:id" render={(rp) => <Stock stocks={stocks} {...rp}/>} />
+        <Route exact path='/stocks'>
+          <Dashboard 
+            stocks={stocks}
+          />
+        </Route>
+
+      </Switch>
     </div>
   );
 }

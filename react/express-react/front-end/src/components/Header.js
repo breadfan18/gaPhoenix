acbdee00f9f2a12bc5e579b0ee914a7e7028c21f
breadfan18 <breadfan18@gmail.com>
2022-01-12 19:54:39 -0500

    added google user photo

--- react/express-react/front-end/src/components/Header.js
+++ react/express-react/front-end/src/components/Header.js
@@ -1,16 +1,28 @@
 import { Link } from "react-router-dom"
 import { login, logout } from "../services/firebase"
 
-const Header = (props) => {
-  return (
-    <nav className="nav">
-      <Link to="/">
-        <div>People App</div>
-      </Link>
-      <button onClick={login}>Login</button>
-      <button onClick={logout}>Logout</button>
-    </nav>
-  )
-}
+const Header = (props) => (
+  <nav className="nav">
+    <Link to="/">
+      <div>People App</div>
+    </Link>
+    {props.user ?
+      <>
+        <button onClick={logout}>Logout</button>
+        <img
+          src={props.user.photoURL}
+          alt=""
+          style={{ 
+              height: '3rem', 
+              width: '3rem', 
+              borderRadius: '50%' 
+          }}
+          />
+      </>
+      :
+      <button onClick={login}>Login</button>}
+
+  </nav>
+)
 
 export default Header

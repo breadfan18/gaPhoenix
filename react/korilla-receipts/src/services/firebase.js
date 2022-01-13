9f36bd548079c9968c5a887320c43f4282e1b641
breadfan18 <breadfan18@gmail.com>
2022-01-12 19:07:25 -0500

    installed firebase config

---
+++ react/korilla-receipts/src/services/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCm-IG7LAc9Pw-JKmyRFYPloFvRs7ghZRs",
+    authDomain: "react-firebase-project-ea97f.firebaseapp.com",
+    projectId: "react-firebase-project-ea97f",
+    storageBucket: "react-firebase-project-ea97f.appspot.com",
+    messagingSenderId: "9327910340",
+    appId: "1:9327910340:web:2899dd9a9b310f3d22d891"
+};
